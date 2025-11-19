234f7c6cf877740d3df0187a6b658435fe9bfc6a
Nirmal Patidar <npatidar@opencrowd.com>
2025-11-19 10:06:59 +0530

    iOS: Setup Project

---
+++ NFCSampleiOS/NFCSampleiOS/NFCSampleiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCSampleiOSApp.swift
+//  NFCSampleiOS
+//
+//  Created by Nirmal Patidar on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NFCSampleiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
